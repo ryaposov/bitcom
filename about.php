d979e9460dab36ecec98f3afcb17e4fe055cad37
Pavel <ryaposov@me.com>
2017-04-17 15:48:51 +0200

    + about page

---
+++ about.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html>
+
+		<!-- Head -->
+		<? require('layout/head.php') ?>
+
+<body>
+
+		<!-- SVG icons include -->
+		<? require('layout/svg.php') ?>
+
+		<!-- Header -->
+		<? require('layout/header.php') ?>
+
+		<section class="main-container">
+			<div class="header-container header-container_content-page">
+				<div class="center">
+					<div class="full-width fl fl-in-di">
+						<div>
+							<ul class="breadcrumbs breadcrumbs_banner">
+								<li><a href="#">Главная</a></li>
+								<li><span>/</span></li>
+								<li><span>О нас</span></li>
+							</ul>
+							<h3 class="header-container__header">О нас</h3>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="center">
+				<div class="content-container about-cnt">
+					<div class="ordinary-note">
+						<p class="ordinary-note__text">Международный деловой центр «Москва-Сити» расположен на Пресненской набережной. Он объединил бизнес, офисы, жилой район и досуг. Где поесть в деловом центре Москва-Сити знают сотрудники здешних офисов, жители этого района и гости. Здесь можно
+							в небольшой кофейне выпить чашку кофе, в пабе – бокал крафтового пива или перекусить в ближайшем фастфуде или пиццерии.</p>
+					</div>
+					<div class="full-width fl fl-in-di">
+						<div class="left">
+							<div class="about-blck about-blck_history">
+								<div class="header-container header-container_bottom-line">
+									<h3 class="header-container__header">Наша история</h3>
+								</div>
+								<div class="about-blck__content">
+									<div class="about-blck_history__point">
+										<span class="about-blck_history__title">
+											<span>Step #1</span>
+											<span class="about-blck_history__label">2010 г.</span>
+										</span>
+										<div class="about-blck_history__text">
+											Along with conventional advertising and below the line activities, organizations and corporate bodies have come to realize that they need to invest in trade shows in order to create maximum recall for their product or brand name.
+										</div>
+									</div>
+									<div class="about-blck_history__point">
+										<span class="about-blck_history__title">
+											<span>Step #1</span>
+											<span class="about-blck_history__label">2010 г.</span>
+										</span>
+										<div class="about-blck_history__text">
+											Along with conventional advertising and below the line activities, organizations and corporate bodies have come to realize that they need to invest in trade shows in order to create maximum recall for their product or brand name.
+										</div>
+									</div>
+									<div class="about-blck_history__point about-blck_history__point_right">
+										<div class="about-blck_history__image">
+											<img src="" alt="" />
+										</div>
+										<span class="about-blck_history__title">
+											<span>Step #1</span>
+											<span class="about-blck_history__label">2010 г.</span>
+										</span>
+										<div class="about-blck_history__text">
+											Along with conventional advertising and below the line activities, organizations and corporate bodies have come to realize that they need to invest in trade shows in order to create maximum recall for their product or brand name.
+										</div>
+									</div>
+									<div class="about-blck_history__point about-blck_history__point_right">
+										<div class="about-blck_history__image">
+											<img src="" alt="" />
+										</div>
+										<span class="about-blck_history__title">
+											<span>Step #1</span>
+											<span class="about-blck_history__label">2010 г.</span>
+										</span>
+										<div class="about-blck_history__text">
+											Along with conventional advertising and below the line activities, organizations and corporate bodies have come to realize that they need to invest in trade shows in order to create maximum recall for their product or brand name.
+										</div>
+									</div>
+									<div class="about-blck_history__point about-blck_history__point_last">
+										<div class="about-blck_history__image">
+											<img src="" alt="" />
+										</div>
+										<span class="about-blck_history__title">
+											<span>Final Step</span>
+											<span class="about-blck_history__label">2010 г.</span>
+										</span>
+										<div class="about-blck_history__text">
+											Along with conventional advertising and below the line activities, organizations and corporate bodies have come to realize that they need to invest in trade shows in order to create maximum recall for their product or brand name.
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="right">
+							<div class="about-blck about-blck_mission">
+								<div class="header-container">
+									<h3 class="header-container__header">Миссия</h3>
+								</div>
+								<div class="about-blck__content">
+									<p>Along with conventional advertising and below the line activities, organizations and corporate bodies have come to realize that they need to invest in trade shows in order to create</p>
+								</div>
+								<div class="about-blck__person">
+									<span class="about-blck__person-image">
+										<img src="" alt="" />
+									</span>
+									<span class="about-blck__person-name">
+										<span class="about-blck__person-first-name">John Smith</span>
+										<span class="about-blck__person-position">Cheif Executive Officer</span>
+									</span>
+								</div>
+							</div>
+							<div class="about-blck about-blck_presentation">
+								<div class="about-blck__content">
+									<span class="about-blck_presentation__title">
+										<span>Презентация Битком24</span>
+										<span class="about-blck_presentation__size">PDF, 2 мб.</span>
+									</span>
+									<a href="#" class="about-blck_presentation__button">Скачать</a>
+								</div>
+							</div>
+							<div class="about-blck about-blck_law-docs">
+								<div class="header-container">
+									<h3 class="header-container__header">Придические документы</h3>
+								</div>
+								<div class="about-blck__content">
+									<ul>
+										<li>
+											<a href="#">Пользовательское соглашение</a>
+										</li>
+										<li>
+											<a href="#">Пользовательское соглашение</a>
+										</li>
+										<li>
+											<a href="#">Пользовательское соглашение</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+							<div class="contact-blck contact-blck_form">
+								<div class="header-container">
+									<h3 class="header-container__header">Напишите нам</h3>
+								</div>
+								<form class="fl fl-in-di">
+									<input class="standart-input" type="text" placeholder="Ваше имя*" />
+									<input class="standart-input" type="text" placeholder="Компания*" />
+									<input class="standart-input" type="text" placeholder="Email*" />
+									<input class="standart-input" type="text" placeholder="Телефон*" />
+									<textarea class="standart-input" placeholder="Сообщение"></textarea>
+									<button>Отправить</button>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- Footer -->
+		<? require('layout/footer.php') ?>
+
+</body>
