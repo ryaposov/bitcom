78c3cca478cb27ccf90117035eee8d30fe325ec3
Pavel <ryaposov@me.com>
2017-04-22 13:51:03 +0200

    bitcom: + sitemap

---
+++ sitemap.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html>
+
+		<!-- Head -->
+		<? require('layout/head.php') ?>
+
+<body>
+
+		<!-- SVG icons include -->
+		<? require('layout/svg.php') ?>
+
+		<!-- Header -->
+		<? require('layout/header.php') ?>
+
+		<section class="main-container">
+			<div class="header-container header-container_content-page header-container_left header-container_noimage">
+				<div class="center">
+					<div class="full-width fl fl-in-di">
+						<div>
+							<ul class="breadcrumbs breadcrumbs_banner">
+								<li><a href="#">Главная</a></li>
+								<li><span>/</span></li>
+								<li><span>Sitemap</span></li>
+							</ul>
+							<h3 class="header-container__header">Sitemap</h3>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="center">
+				<div class="content-container contacts-cnt">
+					<div class="full-width fl fl-in-di">
+						<div class="left sitemap">
+							<div class="content-blck content-blck_sitemap">
+								<div class="header-container">
+									<h3 class="header-container__header">Главная страница</h3>
+								</div>
+								<div class="sitemap__content">
+									<div class="sitemap__row">
+										<div class="sitemap__section">
+											<a href="#" class="sitemap__section-title">Заголовок</a>
+											<ul>
+												<li><a href="#">Рестораны</a></li>
+												<li><a href="#">Бары</a></li>
+												<li><a href="#">Рестораны</a></li>
+												<li><a href="#">Бары</a></li>
+												<li><a href="#">Рестораны</a></li>
+											</ul>
+										</div>
+										<div class="sitemap__section">
+											<a href="#" class="sitemap__section-title">Заголовок</a>
+											<ul>
+												<li><a href="#">Рестораны</a></li>
+												<li><a href="#">Бары</a></li>
+												<li><a href="#">Рестораны</a></li>
+												<li><a href="#">Бары</a></li>
+												<li><a href="#">Рестораны</a></li>
+											</ul>
+										</div>
+										<div class="sitemap__section">
+											<a href="#" class="sitemap__section-title">Заголовок</a>
+											<ul>
+												<li><a href="#">Рестораны</a></li>
+												<li><a href="#">Бары</a></li>
+												<li><a href="#">Рестораны</a></li>
+												<li><a href="#">Бары</a></li>
+												<li><a href="#">Рестораны</a></li>
+											</ul>
+										</div>
+									</div>
+									<div class="sitemap__row">
+										<div class="sitemap__section">
+											<a href="#" class="sitemap__section-title">Заголовок</a>
+											<ul>
+												<li><a href="#">Рестораны</a></li>
+												<li><a href="#">Бары</a></li>
+												<li><a href="#">Рестораны</a></li>
+												<li><a href="#">Бары</a></li>
+												<li><a href="#">Рестораны</a></li>
+											</ul>
+										</div>
+										<div class="sitemap__section">
+											<a href="#" class="sitemap__section-title">Заголовок</a>
+											<ul>
+												<li><a href="#">Рестораны</a></li>
+												<li><a href="#">Бары</a></li>
+												<li><a href="#">Рестораны</a></li>
+												<li><a href="#">Бары</a></li>
+												<li><a href="#">Рестораны</a></li>
+											</ul>
+										</div>
+										<div class="sitemap__section">
+											<a href="#" class="sitemap__section-title">Заголовок</a>
+											<ul>
+												<li><a href="#">Рестораны</a></li>
+												<li><a href="#">Бары</a></li>
+												<li><a href="#">Рестораны</a></li>
+												<li><a href="#">Бары</a></li>
+												<li><a href="#">Рестораны</a></li>
+											</ul>
+										</div>
+									</div>
+									<div class="sitemap__row">
+										<div class="sitemap__section">
+											<a href="#" class="sitemap__section-title">Заголовок</a>
+											<ul>
+												<li><a href="#">Рестораны</a></li>
+												<li><a href="#">Бары</a></li>
+												<li><a href="#">Рестораны</a></li>
+												<li><a href="#">Бары</a></li>
+												<li><a href="#">Рестораны</a></li>
+											</ul>
+										</div>
+										<div class="sitemap__section">
+											<a href="#" class="sitemap__section-title">Заголовок</a>
+											<ul>
+												<li><a href="#">Рестораны</a></li>
+												<li><a href="#">Бары</a></li>
+												<li><a href="#">Рестораны</a></li>
+												<li><a href="#">Бары</a></li>
+												<li><a href="#">Рестораны</a></li>
+											</ul>
+										</div>
+										<div class="sitemap__section">
+											<a href="#" class="sitemap__section-title">Заголовок</a>
+											<ul>
+												<li><a href="#">Рестораны</a></li>
+												<li><a href="#">Бары</a></li>
+												<li><a href="#">Рестораны</a></li>
+												<li><a href="#">Бары</a></li>
+												<li><a href="#">Рестораны</a></li>
+											</ul>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="right">
+							<div class="contact-blck contact-blck_info contact-blck_small">
+								<div class="header-container">
+									<h3 class="header-container__header">Контактная информация</h3>
+								</div>
+								<div class="contact-blck__contact phone">
+			            <a class="phone__phone-number fl fl-ai-c" href="#">8 (495) 255-09-08 </a>
+			            <p class="phone__phone-note">бронировать бесплатно</p>
+			          </div>
+								<div class="contact-blck__contact phone">
+			            <a class="phone__phone-number phone__phone-number_email fl fl-ai-c" href="#">order@bitcom.ru</a>
+			            <p class="phone__phone-note">бронировать бесплатно</p>
+			          </div>
+							</div>
+							<div class="contact-blck contact-blck_form">
+								<div class="header-container">
+									<h3 class="header-container__header">Напишите нам</h3>
+								</div>
+								<form class="fl fl-in-di">
+									<input class="standart-input" type="text" placeholder="Ваше имя*" />
+									<input class="standart-input" type="text" placeholder="Компания*" />
+									<input class="standart-input" type="text" placeholder="Email*" />
+									<input class="standart-input" type="text" placeholder="Телефон*" />
+									<textarea class="standart-input" placeholder="Сообщение"></textarea>
+									<button>Отправить</button>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- Footer -->
+		<? require('layout/footer.php') ?>
+
+</body>
